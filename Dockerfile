aa7d9a9add359aae2f22704f67c291a0def01286
Alex Serra <bounteous17@protonmail.com>
2024-10-09 15:27:02 +0200

    chore: base image form archlinux to ubuntu since aarch64 not supported

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
-FROM archlinux:latest
+FROM ubuntu:24.10
 
-RUN pacman -Sy --noconfirm nodejs sudo docker kubectl aws-cli
+RUN apt update
+RUN apt install -y nodejs sudo curl tar docker.io unzip
+
+COPY install-aws-cli.sh .
+RUN ./install-aws-cli.sh
+
+RUN curl -LO https://dl.k8s.io/release/v1.30.0/bin/linux/arm64/kubectl
+RUN install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 RUN wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
 ENV PNPM_HOME="/home/runner/.local/share/pnpm"
---
+++ install-aws-cli.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Function to detect architecture
+detect_architecture() {
+  ARCH=$(uname -m)
+  if [[ $ARCH == "x86_64" ]]; then
+    echo "amd64"
+  elif [[ $ARCH == "aarch64" ]]; then
+    echo "arm64"
+  else
+    echo "unsupported"
+  fi
+}
+
+# Install AWS CLI based on architecture
+install_aws_cli() {
+  ARCH=$(detect_architecture)
+
+  if [[ $ARCH == "unsupported" ]]; then
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+  fi
+
+  echo "Detected architecture: $ARCH"
+
+  # Set the URL based on the architecture
+  if [[ $ARCH == "amd64" ]]; then
+    CLI_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+  elif [[ $ARCH == "arm64" ]]; then
+    CLI_URL="https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip"
+  fi
+
+  # Download AWS CLI zip
+  echo "Downloading AWS CLI for $ARCH..."
+  curl -o "awscliv2.zip" "$CLI_URL"
+
+  # Unzip the downloaded file
+  echo "Unzipping AWS CLI..."
+  unzip awscliv2.zip
+
+  # Run the install script
+  echo "Installing AWS CLI..."
+  sudo ./aws/install
+
+  # Verify installation
+  aws --version
+
+  # Clean up
+  echo "Cleaning up..."
+  rm -rf awscliv2.zip aws
+}
+
+# Run the install function
+install_aws_cli
